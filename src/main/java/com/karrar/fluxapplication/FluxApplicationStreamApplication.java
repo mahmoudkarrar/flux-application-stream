357cda1a71c3360a3517b2b0727eeb20974715d3
Mahmoud Karrar <mahmod.fathi@gmail.com>
2021-09-02 10:53:50 +0200

    feat: count number of distinct ids per minute

--- src/main/java/com/karrar/fluxapplication/FluxApplicationStreamApplication.java
+++ src/main/java/com/karrar/fluxapplication/FluxApplicationStreamApplication.java
@@ -2,15 +2,6 @@ package com.karrar.fluxapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-import static java.util.Optional.ofNullable;
 
 @SpringBootApplication
 public class FluxApplicationStreamApplication {
