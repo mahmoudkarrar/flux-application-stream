8d2e16f9ff82ad2519f914a5127cc73cd14b876a
Mahmoud Karrar <mahmod.fathi@gmail.com>
2021-09-03 18:05:57 +0200

    refactor: add mvn clean package to start.sh script

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ IMAGE="flux-application-service"
 CONTAINER=flux-application-service
 PORT=8080
 
+mvn clean package
+
 docker kill ${NAME} || echo "Nothing to be stopped, continuing"
 docker rm ${NAME} || echo "Nothing to be removed, continuing"
 
@@ -13,4 +15,5 @@ docker image build --network=host --no-cache -t ${IMAGE} .
 
 docker run -d -p=${PORT}:${PORT} -h ${CONTAINER} --name ${CONTAINER} \
  -e SPRING_PROFILES_ACTIVE=local \
+ -e KINESIS_STREAM_ENABLED="true" \
    ${IMAGE}
